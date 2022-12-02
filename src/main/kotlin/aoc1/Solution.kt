5061814c16e52a17dd6b51e6135688560590f907
Aliaksei Fanski <afanski@gofundme.com>
2022-12-02 11:56:38 -0800

    Fixing paths

--- src/main/kotlin/aoc1/Solution.kt
+++ src/main/kotlin/aoc1/Solution.kt
@@ -2,18 +2,16 @@ package aoc1
 
 import java.io.File
 
-const val INPUT_FILENAME = "/Users/aliakseif/rnd/aoc2022/AoC/src/aoc1.main/kotlin/input_1.txt"
+const val INPUT_FILENAME = "src/main/kotlin/aoc1/input.txt"
 
 fun main() {
     val input = readFile(INPUT_FILENAME);
-    val result = input.split("\n\n").maxOfOrNull { it.split("\n").sumOf { it.toInt() } }
-    println(result)
-}
 
-fun main2() {
-    val input = readFile(INPUT_FILENAME);
-    val result = input.split("\n\n").map { it.split("\n").sumOf { it.toInt() } }
+    val result = input.split("\n\n").maxOfOrNull { it.split("\n").sumOf { it.toInt() } }
+    val result2 = input.split("\n\n").map { it.split("\n").sumOf { it.toInt() } }.sortedDescending().take(3).sum()
+
     println(result)
+    println(result2)
 }
 
 fun readFile(fileName: String): String = File(fileName).readText(Charsets.UTF_8)
