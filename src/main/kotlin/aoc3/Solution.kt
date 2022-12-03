67ac0a7d7511110d4b487829f972731a1dcf1db2
Aliaksei Fanski <afanski@gofundme.com>
2022-12-02 22:53:30 -0800

    Fixing collection type

--- src/main/kotlin/aoc3/Solution.kt
+++ src/main/kotlin/aoc3/Solution.kt
@@ -3,16 +3,13 @@ import java.io.File
 fun main() {
     val input = readFile("src/main/kotlin/aoc3/input.txt")
 
-    val lines = input.split("\n").map {
-        it.split("").subList(1, it.length + 1).let {
-            it.take(it.size / 2).intersect(it.takeLast(it.size / 2))
-        }
-    }.map { it.first() }.map { it.toCharArray().first().code }.map { if (it >= 97) it - 96 else it - 38 }.sum()
+    val result1 = input.split("\n").map {it.toCharArray().let { it.take(it.size / 2).intersect(it.takeLast(it.size / 2)) } }.map { it.first().code }.map { if (it >= 97) it - 96 else it - 38 }.sum()
 
-    println(lines)
-    println('A'.code)
-    println('a'.code)
-    println('G'.code)
+    println(result1)
+
+    val result2 = input.split("\n").chunked(3).map { it[0].toCharArray().intersect(it[1].toCharArray().asIterable()).intersect(it[2].toCharArray().asIterable()) }.map { it.first().code }.map { if (it >= 97) it - 96 else it - 38 }.sum()
+
+    println(result2)
 }
 
 fun readFile(fileName: String): String = File(fileName).readText(Charsets.UTF_8)
