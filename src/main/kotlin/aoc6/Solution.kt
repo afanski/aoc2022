2c154de6267be45f7270503e5c92968d448d7d61
Aliaksei Fanski <afanski@gofundme.com>
2022-12-05 21:21:53 -0800

    Formatting

--- src/main/kotlin/aoc6/Solution.kt
+++ src/main/kotlin/aoc6/Solution.kt
@@ -5,14 +5,13 @@ import java.io.File
 fun main() {
     val input = readFile("src/main/kotlin/aoc6/input.txt")
 
-    val result = input.split("").windowed(4, 1).let { it.takeLast(it.size - 1) }. first {
-        it.distinct().size == it.size
-    }.joinToString("").let { input.indexOf(it) + it.length }
+    val result = input.split("").windowed(4, 1).let { it.takeLast(it.size - 1) }.first { it.distinct().size == it.size }
+        .joinToString("").let { input.indexOf(it) + it.length }
     println(result)
 
-    val result2 = input.split("").windowed(14, 1).let { it.takeLast(it.size - 1) }. first {
-        it.distinct().size == it.size
-    }.joinToString("").let { input.indexOf(it) + it.length }
+    val result2 =
+        input.split("").windowed(14, 1).let { it.takeLast(it.size - 1) }.first { it.distinct().size == it.size }
+            .joinToString("").let { input.indexOf(it) + it.length }
     println(result2)
 }
 
