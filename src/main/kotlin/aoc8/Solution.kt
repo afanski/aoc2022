e473bdc7df62a527cb63405b0fffd9621fc1d788
Aliaksei Fanski <afanski@gofundme.com>
2022-12-07 22:05:56 -0800

    Day 8 part 2 cleanup

--- src/main/kotlin/aoc8/Solution.kt
+++ src/main/kotlin/aoc8/Solution.kt
@@ -1,8 +1,6 @@
 package aoc8
 
 import java.io.File
-import java.lang.Math.max
-import java.lang.Math.min
 
 fun main() {
     val input = readFile("src/main/kotlin/aoc8/input.txt")
@@ -25,28 +23,28 @@ fun main() {
                 visible[i][j] = 1
             } else {
                 var visibleLeft = true
-                for (iter in 0..i-1) {
+                for (iter in 0 until i) {
                     if (matrix[iter][j] >= matrix[i][j]) {
                         visibleLeft = false
                         break
                     }
                 }
                 var visibleRight = true
-                for (iter in 0..j-1) {
+                for (iter in 0 until j) {
                     if (matrix[i][iter] >= matrix[i][j]) {
                         visibleRight = false
                         break
                     }
                 }
                 var visibleTop = true
-                for (iter in i+1..lines.size - 1) {
+                for (iter in i+1 until lines.size) {
                     if (matrix[iter][j] >= matrix[i][j]) {
                         visibleTop = false
                         break
                     }
                 }
                 var visibleBottom = true
-                for (iter in j+1..lines.size - 1) {
+                for (iter in j+1 until lines.size) {
                     if (matrix[i][iter] >= matrix[i][j]) {
                         visibleBottom = false
                         break
@@ -57,33 +55,31 @@ fun main() {
         }
     }
 
-
-    println(visible)
     val result = visible.map { it.sum()  }.sum()
     println(result)
 
     val distances: Array<IntArray> = Array(lines.size) { IntArray(lines.size) }
     matrix.forEachIndexed { i, e ->
         e.forEachIndexed { j, e2 ->
-            var il = max(i - 1, 0)
+            var il = kotlin.math.max(i - 1, 0)
             while (il > 0 && matrix[il][j] < matrix[i][j]) {
                 il--
             }
             val distanceLeft = i - il
 
-            var ir = min(i + 1, lines.size - 1)
+            var ir = kotlin.math.min(i + 1, lines.size - 1)
             while (ir < lines.size - 1 && matrix[ir][j] < matrix[i][j]) {
                 ir++
             }
             val distanceRight = ir - i
 
-            var jt = max(j - 1, 0)
+            var jt = kotlin.math.max(j - 1, 0)
             while (jt > 0 && matrix[i][jt] < matrix[i][j]) {
                 jt--
             }
             val distanceTop = j - jt
 
-            var jb = min(j + 1, lines.size - 1)
+            var jb = kotlin.math.min(j + 1, lines.size - 1)
             while (jb < lines.size - 1 && matrix[i][jb] < matrix[i][j]) {
                 jb++
             }
@@ -93,7 +89,7 @@ fun main() {
         }
     }
 
-    val result2 = distances.map { it.max() }.max()
+    val result2 = distances.maxOfOrNull { it.max() }
     println(result2)
 }
 
