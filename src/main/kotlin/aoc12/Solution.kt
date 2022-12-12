7647f3ae0ef57f36984604d8af8ef3a7aa817c9a
Aliaksei Fanski <afanski@gofundme.com>
2022-12-12 10:35:41 -0800

    Day 12 Part 2

--- src/main/kotlin/aoc12/Solution.kt
+++ src/main/kotlin/aoc12/Solution.kt
@@ -16,7 +16,8 @@ fun main() {
     val input = readFile("src/main/kotlin/aoc12/input.txt")
     val rows = input.split("\n")
     array = Array(rows.size) { IntArray(rows.first().length) }
-    val distances = Array(rows.size) { IntArray(rows.first().length) }
+
+
     X = rows.size
     Y = rows.first().length
 
@@ -25,7 +26,7 @@ fun main() {
             r.forEachIndexed { index2, h ->
                 run {
                     if (h == 'S') {
-                        array[index][index2] = 0
+                        array[index][index2] = 1
                     } else if (h == 'E') {
                         array[index][index2] = 'z'.code - 'a'.code + 2
                     }
@@ -50,10 +51,32 @@ fun main() {
         }
     }
 
+    var starts = mutableSetOf<Pair<Int, Int>>()
+    rows.forEachIndexed { index, r ->
+        run {
+            r.forEachIndexed { index2, h ->
+                run {
+                    if (h == 'S' || h == 'a') {
+                        starts.add(Pair(index, index2))
+                    }
+                }
+            }
+        }
+    }
+
+    println(array)
+    val minDistances = starts.map { processPath(it) }
+
+    println(minDistances.filter { it > 0 }.min())
+//    println(findPath(startX, startY, 0, mutableSetOf(), mutableListOf()))
+}
+
+fun processPath(start: Pair<Int, Int>): Int {
     val queue = mutableListOf<Pair<Int, Int>>()
     val seen = mutableSetOf<Pair<Int, Int>>()
+    val distances = Array(X) { IntArray(Y) }
 
-    queue.add(Pair(startX, startY))
+    queue.add(Pair(start.first, start.second))
 
     while (queue.isNotEmpty()) {
         val current = queue.removeFirst()
@@ -66,15 +89,9 @@ fun main() {
         process(current.first - 1, current.second, seen, queue, distances, currentDistance, current)
         process(current.first, current.second + 1, seen, queue, distances, currentDistance, current)
         process(current.first, current.second - 1, seen, queue, distances, currentDistance, current)
-
     }
 
-    println(array)
-    println(('S'.code - 'a'.code + 1))
-    println(('E'.code - 'a'.code + 1))
-    println(('E'.code - 'a'.code + 1))
-    println(distances[endX][endY])
-//    println(findPath(startX, startY, 0, mutableSetOf(), mutableListOf()))
+    return distances[endX][endY]
 }
 
 private fun process(
@@ -86,7 +103,6 @@ private fun process(
     distance: Int,
     current: Pair<Int, Int>
 ) {
-    println("process $i and $j")
     if (checkIJ(i, j) && array[i][j] - array[current.first][current.second] <= 1) {
         distances[i][j] = distance + 1
         if (!seen.contains(Pair(i, j))) {
@@ -96,61 +112,6 @@ private fun process(
     }
 }
 
-fun findPath(i: Int, j: Int, count: Int, seenOld: MutableSet<Pair<Int, Int>>, pathOld: MutableList<String>): Int? {
-    if (pathOld.size > min) {
-        return null
-    }
-    if (i == endX && j == endY) {
-        println("Path : $pathOld, count: ${pathOld.size}" )
-        if (pathOld.size < min) {
-            min = pathOld.size
-        }
-        return pathOld.size
-    }
-
-    val newCount = count + 1
-    var results = mutableSetOf<Int>()
-
-    seenOld.add(Pair(i, j))
-    var seen = seenOld.toMutableSet()
-
-    if (checkIJ(i - 1, j) && !seenOld.contains(Pair(i - 1, j)) && array[i-1][j] - array[i][j] <= 1) {
-        var path = pathOld.toMutableList()
-        path.add("^" + array[i-1][j] + " - " + newCount)
-
-        seen.add(Pair(i - 1, j))
-        findPath(i - 1, j, newCount, seen, path)?.let {
-            results.add(it)
-        }
-    }
-
-    if (checkIJ(i + 1, j) && !seenOld.contains(Pair(i + 1, j)) && array[i+1][j] - array[i][j] <= 1) {
-        var path = pathOld.toMutableList()
-        path.add("V" + array[i+1][j] + " - " + newCount)
-        seen.add(Pair(i + 1, j))
-        findPath(i + 1, j, newCount, seen, path)?.let {
-            results.add(it)
-        }
-    }
-    if (checkIJ(i, j - 1) && !seenOld.contains(Pair(i, j - 1)) && array[i][j-1] - array[i][j] <= 1) {
-        var path = pathOld.toMutableList()
-        path.add("<" + array[i][j-1] + " - " + newCount)
-        seen.add(Pair(i, j - 1))
-        findPath(i, j - 1, newCount, seen, path)?.let {
-            results.add(it)
-        }
-    }
-    if (checkIJ(i, j + 1) && !seenOld.contains(Pair(i, j + 1))&& array[i][j+1] - array[i][j] <= 1) {
-        var path = pathOld.toMutableList()
-        path.add(">"  + array[i][j+1] + " - " + newCount)
-        seen.add(Pair(i, j + 1))
-        findPath(i, j + 1, newCount, seen, path)?.let {
-            results.add(it)
-        }
-    }
-
-    return results.minOrNull()
-}
 
 fun checkIJ(i: Int, j: Int): Boolean {
     return !(i < 0 || j < 0 || i >= X || j >= Y)
