994ab9955a6b1fea722087b0a44703543467e867
Aliaksei Fanski <afanski@gofundme.com>
2022-12-14 09:14:10 -0800

    Day 14 parsing

--- src/main/kotlin/aoc14/Solution.kt
+++ src/main/kotlin/aoc14/Solution.kt
@@ -4,7 +4,7 @@ import java.io.File
 import kotlin.math.sign
 
 fun main() {
-    val input = readFile("src/main/kotlin/aoc14/input.txt")
+    val input = readFile("src/main/kotlin/aoc14/input2.txt")
     val result = input.split("\n").map { it.split(" -> ").map { it.split(",").map { it.toInt() } } }
     val xMin = result.map { it.map { it.first() } }.map { it.min() }.min()
     val xMax = result.map { it.map { it.first() } }.map { it.max() }.max()
@@ -35,9 +35,47 @@ fun main() {
         }
     }
 
+    var startX = 500 - xMin
+    var startY = 0
+    array[startY][startX] = 2
+
     array.forEach {
         println(it.joinToString(""))
     }
+
+    while (nextPosition(startX, startY, array) != null) {
+        val next = nextPosition(startX, startY, array)
+        array[startY][startX] = 0
+        array[next!!.second][next!!.first] = 2
+        startX = next.first
+        startY = next.second
+    }
+
+    array.forEach {
+        println(it.joinToString(""))
+    }
+}
+
+fun nextPosition(startX: Int, startY: Int, array: Array<IntArray>): Pair<Int, Int>? {
+    if (startY + 1 >= array.size) {
+        return null
+    } else if (array[startY + 1][startX] == 0) {
+        return Pair(startX, startY + 1)
+    }
+
+    if (startX - 1 < 0) {
+        return null
+    } else if (array[startY + 1][startX - 1] == 0) {
+        return Pair(startX - 1, startY + 1)
+    }
+
+    if (startX + 1 > array.first().size) {
+        return null
+    } else if (array[startY + 1][startX + 1] == 0) {
+        return Pair(startX + 1, startY + 1)
+    }
+
+    return null
 }
 
 fun readFile(fileName: String): String = File(fileName).readText(Charsets.UTF_8)
